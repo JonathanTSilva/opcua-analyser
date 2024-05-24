8ea5b7bf8c130cad7c6793725a8b224ca7a4588a
jonathan.silva <jonathan.silva@smar.com.br>
2024-05-24 11:59:12 -0300

    refactor: edit data paths

--- uanalyser/paths.py
+++ uanalyser/paths.py
@@ -10,7 +10,8 @@ __all__ = [
     'TEMP',
     'ROOT',
     'LOG',
-    'DATA',
+    'DATA_PCAPNG',
+    'DATA_PERF',
     'TESTS',
     'TESTS_ASSETS',
     'DOCS',
@@ -21,7 +22,8 @@ UANALYSER = os.path.dirname(os.path.abspath(__file__))
 ROOT = os.path.dirname(UANALYSER)
 TEMP = tempfile.gettempdir()
 LOG = os.path.join(UANALYSER, 'uanalyser.log')
-DATA = os.path.join(ROOT, 'Data/network_traffic/no_filter')
+DATA_PCAPNG = os.path.join(ROOT, 'Data/network_traffic/no_filter')
+DATA_PERF = os.path.join(ROOT, 'Data/get_performance')
 TESTS = os.path.join(ROOT, 'tests')
 TESTS_ASSETS = os.path.join(TESTS, 'assets')
 DOCS = os.path.join(ROOT, 'docs')

a43769768117e0e011bc27050342c2b0d17b404d
jonathan.silva <jonathan.silva@smar.com.br>
2024-07-30 07:59:17 -0300

    test: update the project

---
+++ tests/test_cli.py
@@ -0,0 +1,8 @@
+# from typer.testing import CliRunner
+
+# runner = CliRunner()
+
+# def test_analyse_package_cli_must_return_0_in_stdout():
+#     result = runner.invoke(analyse_package)
+#     assert result.exit_code == 0
+#     assert result.stdout == "['normal_traffic']\n"
